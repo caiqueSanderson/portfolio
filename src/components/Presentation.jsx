67165213c98c0e237ff37964b10ece5297efc58e
sanderson <cbsanderson.04@gmail.com>
2024-08-18 17:49:58 -0300

    Finilizado o componente Presentation.jsx

--- src/components/Presentation.jsx
+++ src/components/Presentation.jsx
@@ -9,14 +9,14 @@ export default function Presentation(){
                     <div className="links">
                         <a href="#">projetos</a>
                         <a href="#">cv</a>
-                        <a href="#"><img src="public/github.svg" alt="Logo Github" /></a>
-                        <a href="#"><img src="public/linkedin.svg" alt="Logo Linkedin" /></a>
+                        <a href="www.linkedin.com/in/caique-sanderson-de-sá-borges-262545237"><img className="icons_contact" src="public/linkedin.svg" alt="Logo Linkedin" /></a>
+                        <a href="https://github.com/caiqueSanderson"><img className="icons_contact" src="public/github.svg" alt="Logo Github" /></a>
                     </div>
                 </header>
-                <div>
-                    <h2>&lt;h2&gt;Hello world&lt;/h2&gt;</h2>
+                <div className="text">
+                    <h2>Hello world!</h2>
                     <h1>Meu nome é <span className="bold">Caique Sanderson</span></h1>
-                    <p>Sou um &lt;p&gt;desenvolvedor&lt;/p&gt;</p>
+                    <p>Sou um &lt; desenvolvedor / &gt;</p>
                 </div>
             </div>
         </>
